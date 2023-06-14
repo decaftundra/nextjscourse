deb134f29e0cee50c134ff80e3fbf175f616a175
Julien Beghain <julienbeghain@gmail.com>
2023-06-14 17:37:05 +0100

    gitignore

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "3bd182c8ad27828a"; }
+/******/ 		__webpack_require__.h = function() { return "1fba62eb1d797d85"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
