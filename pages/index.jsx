ba6359cbd7c093bf5cc97aa77394a3c3da9c746d
Julien Beghain <julienbeghain@gmail.com>
2023-10-12 18:30:26 +0100

    77. Styling Components In Next.js Projects

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,8 +1,12 @@
-import Link from "next/link";
+import { getFeaturedEvents } from '../dummy-data';
+import EventList from '../components/events/event-list';
+
 function HomePage() {
+  const featuredEvents = getFeaturedEvents();
+
   return (
     <div>
-      <h1>Home Page</h1>
+        <EventList items={featuredEvents} />
     </div>
   );
 }
